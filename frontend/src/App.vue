ece109888b4e5483c2d5a6af81811e37c7447dad
Magic <2525egawo@gmail.com>
2025-07-07 05:04:45 +0900

    ログイン状態をPiniaで永続化・復元できるように修正

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,18 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import Navbar from '@/components/Navbar.vue'
+import { onMounted } from 'vue'
+
 
 const userStore = useUserStore()
 
 function logout() {
   userStore.clearUser()
 }
+onMounted(() => {
+  userStore.fetchUser()  // ✅ ログイン済みならここで状態が復元される
+})
+
 </script>
 
 <template>
