82cd39a1aef0e7c65ed9ad9e925b1387dde0f0c9
Magic <2525egawo@gmail.com>
2025-07-19 14:01:41 +0900

    退会後にログアウト処理を実行するようGoodbyeComplete.vueを更新

--- frontend/src/views/account/GoodbyeComplete.vue
+++ frontend/src/views/account/GoodbyeComplete.vue
@@ -1,10 +1,18 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+
+onMounted(() => {
+  userStore.logout()
+})
+</script>
+
 <template>
-  <div class="max-w-lg mx-auto p-8 text-center">
-    <h1 class="text-2xl font-bold text-gray-800 mb-4">ご利用ありがとうございました</h1>
-    <p class="text-gray-600 mb-6">
-      アカウントの削除が完了しました。<br>
-      またのご利用をお待ちしております。
-    </p>
-    <router-link to="/" class="text-blue-600 hover:underline">トップページへ戻る</router-link>
+  <div class="p-8 max-w-xl mx-auto text-center">
+    <h1 class="text-2xl font-bold mb-4">退会が完了しました</h1>
+    <p class="text-gray-600 mb-6">ご利用ありがとうございました。</p>
+    <router-link to="/" class="text-blue-600 hover:underline">ホームに戻る</router-link>
   </div>
 </template>
